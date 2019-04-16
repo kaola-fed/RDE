924c7323ae6d0ececa4de9d00d9bf60df6583b34
wangsong <wangsong3635@outlook.com>
2019-04-16 16:41:53 +0800

    feat: update template create

--- src/commands/template/create.ts
+++ src/commands/template/create.ts
@@ -1,10 +1,8 @@
 import {exec} from 'child_process'
-import cli from 'cli-ux'
 import * as fs from 'fs'
-// @ts-ignore
-import {ncp} from 'ncp'
 import * as path from 'path'
-import * as process from 'process'
+// @ts-ignore
+import * as copy from 'recursive-copy'
 import * as util from 'util'
 // @ts-ignore
 import * as writePkgJson from 'write-pkg'
@@ -26,75 +24,36 @@ export default class Create extends Base {
 
   static args = [{
     name: 'rdtName',
-    required: false,
-    description: 'package name, used by package.json',
+    required: true,
+    description: 'rde template project name, used by package.json',
   }]
 
   readonly baseRdtName = '@rede/rdt-helloworld'
 
   private rdtName = ''
 
-  public async initialize() {
-    await this.prompt()
-
-    await asyncExec(`mkdir ${this.rdtName}`)
-
-    process.chdir(path.resolve(process.cwd(), this.rdtName))
-
-    logger.info(`Installing ${this.rdtName}. This might take a while...`)
-
-    await writePkgJson({name: this.rdtName})
-
-    await npm.install(this.baseRdtName)
-
-    await asyncExec('rm package*.json')
+  get rdtPkgDir() {
+    return path.resolve(this.cwd, 'node_modules', this.baseRdtName)
   }
 
-  getBaseRdtProjectDir() {
-    const cwd = process.cwd()
-    return path.resolve(cwd, 'node_modules', this.baseRdtName)
-  }
-
-  async render() {
-    const cwd = process.cwd()
-    const srcDir = path.resolve(this.getBaseRdtProjectDir(), 'template')
-
-    const destDir = path.resolve(cwd, 'template')
-
-    const rdtConf = require(path.resolve(this.getBaseRdtProjectDir(), conf.getRdtConfName()))
-    await render.renderDir(srcDir, {
-      ProjectName: this.rdtName
-    }, rdtConf.render.includes, destDir, rdtConf.render.tags)
-  }
-
-  async run() {
-    const cwd = process.cwd()
-    const srcDir = path.resolve(cwd, 'node_modules', this.baseRdtName)
-    const destDir = path.resolve(cwd)
-
-    await ncp(srcDir, destDir)
-  }
-
-  public async postRun() {
-    logger.complete('Created project')
-    logger.star('Start with command:')
-    logger.star('$ rde run serve')
-  }
-
-  private async prompt() {
-    this.rdtName = await cli.prompt('template name: (rdt-{name})', {
-      required: true
-    })
-
-    if (!this.rdtName.includes('rdt-')) {
+  public async preInit() {
+    const {args} = this.parse(Create)
+    // check rdtName start with rdt-
+    if (args.rdtName.includes('rdt-')) {
+      this.rdtName = args.rdtName
+    } else {
       this.rdtName = `rdt-${this.rdtName}`
     }
 
-    if (fs.existsSync(path.resolve(process.cwd(), this.rdtName))) {
+    await asyncExec(`rm -rf ${path.resolve(this.cwd, this.rdtName)}`)
+
+    // check rdtName directory is not existed
+    if (fs.existsSync(path.resolve(this.cwd, this.rdtName))) {
       logger.error(`${this.rdtName} directory is already existed`)
       this.exit(1)
     }
 
+    // check npm rdtName module is not existed
     try {
       await npm.getInfo(this.rdtName)
       logger.error(`npm module ${this.rdtName} is existed, please rename and try again`)
@@ -105,5 +64,72 @@ export default class Create extends Base {
         this.exit(1)
       }
     }
+  }
+
+  public async initialize() {
+    await asyncExec(`mkdir ${this.rdtName}`)
+
+    process.chdir(this.rdtName)
+
+    logger.info(`Installing ${this.baseRdtName}. This might take a while...`)
+
+    await writePkgJson({name: this.rdtName})
+
+    await npm.install(this.baseRdtName)
+
+    await asyncExec('rm package*.json')
+  }
+
+  async render() {
+    await Promise.all([
+      this.renderTemplate(),
+      this.copyPkgToCwd('app', 'app'),
+      this.copyPkgToCwd('rde.template.js', 'rde.template.js'),
+      this.copyPkgToCwd('.npmignore', '.gitignore'),
+      this.renderOther()
+    ])
+  }
+
+  async renderTemplate() {
+    const srcDir = path.resolve(this.rdtPkgDir, 'template')
+
+    const destDir = path.resolve(this.cwd, 'template')
+
+    const rdtConf = require(path.resolve(this.rdtPkgDir, conf.getRdtConfName()))
+    await render.renderDir(srcDir, {
+      ProjectName: this.rdtName
+    }, ['.json', '.html', '.js'], destDir, rdtConf.render.tags)
+  }
+
+  async copyPkgToCwd(src: string, dest: string) {
+    const srcDir = path.resolve(this.rdtPkgDir, src)
+    const destDir = path.resolve(this.cwd, dest)
+
+    await copy(srcDir, destDir)
+  }
+
+  async renderOther() {
+    // package.json: 赋值name,description,keywords,除去下划线开头的配置
+    const pkgConfig = require(path.resolve(this.rdtPkgDir, 'package.json'))
+    pkgConfig.name = this.rdtName
+    pkgConfig.description = `${this.rdtName} rede-template`
+    pkgConfig.keywords = [this.rdtName, 'rede-template']
+    Object.keys(pkgConfig).forEach(item => {
+      if (item[0] === '_') {
+        delete pkgConfig[item]
+      }
+    })
+    writePkgJson(pkgConfig)
+  }
+
+  async run() {
+    await asyncExec('rm -rf node_modules')
+    await npm.install('', false, this.cwd)
+  }
+
+  public async postRun() {
+    logger.complete('Created project')
+    logger.star('Start with command:')
+    logger.star('$ rde run serve')
   }
 }
