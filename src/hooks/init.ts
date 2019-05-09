ab79930ec6c583c306230a515fde6bdbb73970a8
normalHamal <3292106931@qq.com>
2019-05-09 10:28:04 +0800

    fix: update hooks bug

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,6 +1,6 @@
 import * as Table from 'cli-table2'
 import * as semver from 'semver'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
 import * as path from 'path'
 
 import npm from '../services/npm'
@@ -9,47 +9,45 @@ export default async function({ config }) {
   const file = path.join(config.cacheDir, 'version')
   const delayInDays = 1
 
-  const checkVersionFile = () => {
-    if(!fs.existsSync(config.cacheDir)) {
-      fs.mkdirSync(config.cacheDir)
-    }
-
-    if(!fs.existsSync(file)) {
-      fs.writeFileSync(file, JSON.stringify({
-        current: config.version
-      }))
-    }
-  }
-
-  const needCheck = () => {
+  const needCheck = async () => {
     try {
-      const { mtime } = fs.statSync(file)
-      const expireDate = new Date(mtime.valueOf() + 1000 * 60 * 60 * 24 * delayInDays)
+      const { mtime } = await fs.stat(file)
+      const expireDate = new Date(
+        mtime.valueOf() + 1000 * 60 * 60 * 24 * delayInDays
+      )
       return expireDate < new Date()
     } catch (e) {
-      if(e) {
-        checkVersionFile()
-      }
+      if (e.code !== 'ENOENT') throw e
       return true
     }
   }
 
-  const checkVersion = () => {
-    const { latest } = JSON.parse(fs.readFileSync(file, 'utf8'))
+  const checkVersion = async () => {
+    const { latest } = await fs.readJson(file)
+
+    if(!latest) await updateVersion()
+
     const table = new Table({
       head: ['package', 'current', 'latest'],
       style: {
-        'padding-left': 1
-        , 'padding-right': 1
-        , head: ['cyan']
-        , border: ['white']
+        'padding-left': 1,
+        'padding-right': 1,
+        head: ['cyan'],
+        border: ['white']
       },
       colWidths: [15, 25, 25]
     })
 
-    if(semver.gt(latest, config.version)) {
+    if (semver.gt(latest, config.version)) {
       table.push([config.name, config.version, latest])
-      table.push([{content: `New version available. Please enter 'npm update -g ${config.name}' to update`, colSpan:3}])
+      table.push([
+        {
+          content: `New version available. Please enter 'npm update -g ${
+            config.name
+          }' to update`,
+          colSpan: 3
+        }
+      ])
 
       console.log(table.toString())
     }
@@ -58,13 +56,16 @@ export default async function({ config }) {
   const updateVersion = async () => {
     const pkgInfo = await npm.getInfo(config.name)
 
-    fs.writeFileSync(file, JSON.stringify({
-      ...pkgInfo['dist-tags'],
-      current: config.version
-    }))
+    await fs.outputFile(
+      file,
+      JSON.stringify({
+        ...pkgInfo['dist-tags'],
+        current: config.version
+      })
+    )
   }
 
-  if(needCheck()) {
+  if (await needCheck()) {
     await updateVersion()
   }
 
