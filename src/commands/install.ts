146b524d0954107fb62709e156b6e67d2c77586b
hale <hzhayue@corp.netease.com>
2019-06-05 20:00:03 +0800

    feat: save

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -1,7 +1,11 @@
+import * as path from 'path'
+
 import Base from '../base'
 import RunBase from '../base/run'
+import conf from '../services/conf'
 import sync from '../services/sync'
 
+const {resolve} = path
 export default class Install extends RunBase {
   public static strict = false
 
@@ -15,6 +19,16 @@ export default class Install extends RunBase {
   }
 
   public async run() {
+    if (conf.isApp) {
+      const {cwd, localCacheDir} = conf
+
+      await sync.mergePkgJson(
+        resolve(cwd, 'app', 'package.json'),
+        resolve(cwd, localCacheDir, 'package.json'),
+        resolve(localCacheDir),
+      )
+    }
+
     await sync.install()
   }
 }
