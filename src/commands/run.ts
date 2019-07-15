a2f9c51a69c133aeeef51347589506d9c46c03b7
normalHamal <3292106931@qq.com>
2019-07-15 11:07:06 +0800

    fix: windows bugs

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -133,13 +133,8 @@ export default class Run extends RunBase {
       stdio: 'pipe'
     })
 
-    child.stdout.on('data', data => {
-      console.log(data)
-    })
-
-    child.stderr.on('data', data => {
-      console.log(data)
-    })
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stdout)
 
     child.on('close', code => {
       if (code !== 0) {
